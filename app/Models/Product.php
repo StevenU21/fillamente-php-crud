3c59db0ef0fe0cf9fa1ce7424128a503d4f61de0
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-03-07 20:47:01 -0600

    add product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock',
+        'image'
+    ];
+
+    public function getImageUrlAttribute(): string
+    {
+        return asset('storage/' . $this->logo);
+    }
 }
